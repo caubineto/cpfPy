1a6a7a6e4ac2aa22319fb19c2f2dba214a879b38
Caubi Neto <caubineto@outlook.com>
2023-09-29 14:02:36 -0300

    código melhorado

--- cpfPy.py
+++ cpfPy.py
@@ -1,11 +1,35 @@
-cpf = '21512449032'
-noveDigitos = cpf[:9]
+import os
+
+cpfEnviadoUsuario = input('Digite seu CPF abaixo. (Apenas números)\n')
+noveDigitos = cpfEnviadoUsuario[:9]
 contadorRegressivo1 = 10
 
-resultadoDigito1 = 0
-for digito1 in noveDigitos:
-    resultadoDigito1 += (int(digito1) * contadorRegressivo1)
-    contadorRegressivo1 -= 1
-digito1 = ((resultadoDigito1 * 10) % 11)
-digito1 = digito1 if digito1 <= 9 else 0
-print(digito1)
+try:
+    resultadoDigito1 = 0
+    for digito in noveDigitos:
+        resultadoDigito1 += (int(digito) * contadorRegressivo1)
+        contadorRegressivo1 -= 1
+    digito1 = ((resultadoDigito1 * 10) % 11)
+    digito1 = digito1 if digito1 <= 9 else 0
+
+    dezDigitos = noveDigitos + str(digito1)
+    contadorRegressivo2 = 11
+
+    resultadoDigito2 = 0
+    for digito in dezDigitos:
+        resultadoDigito2 += int(digito) * contadorRegressivo2
+        contadorRegressivo2 -= 1
+    digito2 = ((resultadoDigito2 * 10) % 11)
+    digito2 = digito2 if digito2 <= 9 else 0
+
+    cpfGeradoCalculo = (f'{noveDigitos}{digito1}{digito2}')
+
+    if cpfEnviadoUsuario == cpfGeradoCalculo:
+        print(f'{cpfEnviadoUsuario} é válido')
+    else:
+        print('CPF inválido')
+    
+except ValueError:
+    os.system('cls')
+    print(f'Digite apenas números, sem "." ou "-"')
+
